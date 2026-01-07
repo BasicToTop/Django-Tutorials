3ceb83b230e7d81550268b9ffe63077fda6e27f5
errampy <rampython2019@gmail.com>
2026-01-07 19:50:30 +0300

    finished teh forms validation end toend

--- pro6/testapp/forms.py
+++ pro6/testapp/forms.py
@@ -1,4 +1,16 @@
 from django import forms
+from django.core import validators
+
+
+def email_validator(value):
+    email_extention = 'basictotop.in'
+    # test@gmail.com
+    splited_email = value.split('@')  # ['test', 'gmail.com']
+    print('splited_email')
+    if email_extention not in splited_email:
+        raise forms.ValidationError('only basictotop.in mail can allowed here')
+    else:
+        return value
 
 
 class ContactUsForm(forms.Form):
@@ -8,19 +20,24 @@ class ContactUsForm(forms.Form):
         ('female', 'Female'),
         ('other', 'Other'),
     )
-    name = forms.CharField(max_length=50)
-    email = forms.EmailField(max_length=50)
+    name = forms.CharField(max_length=5, validators=[
+                           validators.MinLengthValidator(2)])
+    email = forms.EmailField(max_length=50, validators=[email_validator])
     phone_number = forms.CharField(max_length=15)
     gender = forms.ChoiceField(choices=GENDER_CHOICE)
+    message = forms.CharField(widget=forms.Textarea, validators=[
+                              validators.MaxLengthValidator(10),
+                              validators.MinLengthValidator(5)])
+    # dob = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
 
     # form validation by using clean method
 
-    def clean_name(self):
-        name = self.cleaned_data['name']
+    # def clean_name(self):
+    #     name = self.cleaned_data['name']
 
-        if len(name) <= 1:
-            raise forms.ValidationError('Name must be more that 1 character')
-        return name
+    #     if len(name) <= 1:
+    #         raise forms.ValidationError('Name must be more that 1 character')
+    #     return name
 
     def clean_phone_number(self):
         phone_number = self.cleaned_data['phone_number']
