9b4b6a1efb981f5c4bf688d06760199e03593d7c
errampy <rampython2019@gmail.com>
2026-02-02 19:23:19 +0300

    explaiend contains, exact along with i and templates

---
+++ pro7/testapp/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+class EmployeeForm(forms.Form):
+    first_name = forms.CharField(max_length=30, label='First Name', required=False)
+    # middle_name = forms.CharField(max_length=30, required=False, label='Middle Name')
+    # last_name = forms.CharField(max_length=30, label='Last Name')
+    email = forms.EmailField(label='Email Address', required=False)
+    # phone_number = forms.CharField(max_length=15, label='Phone Number')
+    # position = forms.CharField(max_length=50, label='Position')
+    # doj = forms.DateField(label='Date of Joining', widget=forms.SelectDateWidget)
+    # salary = forms.FloatField(label='Salary')
+    # is_active = forms.BooleanField(required=False, initial=True, label='Is Active')
--- pro7/testapp/views.py
+++ pro7/testapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from testapp.models import Employee
+from testapp.forms import EmployeeForm
 # Create your views here.
+
+
+def employee_list(request):
+    form = EmployeeForm()
+    first_name = request.GET.get('first_name')
+    email = request.GET.get('email')
+    if first_name:
+        records = Employee.objects.filter(first_name__iexact=first_name).order_by('first_name')
+    elif email:
+        records = Employee.objects.filter(email__iexact=email).order_by('first_name')
+    else:
+        records = Employee.objects.all().order_by('first_name') # Fetch all employee records
+    context = {'employees': records, 'form': form}
+    template_name = 'testapp/employee_list.html'
+    return render(request, template_name, context)
